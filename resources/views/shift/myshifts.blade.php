3ae47b9eea8f3cfa0e3cec6e2d6e0b924240eb23
Anders <ande7195@edu.easj.dk>
2018-12-07 21:02:55 +0100

    MyShifts Created

---
+++ resources/views/shift/myshifts.blade.php
@@ -0,0 +1,20 @@
+@extends('layout')
+
+
+@section('content')
+
+<table class="table">
+    <thead>
+    <tr>
+        <th>Indstemplet</th>
+        <th>Udstemplet</th>
+        <th>Pause</th>
+        <th>Startidspunkt</th>
+        <th>Sluttidspunk</th>
+        <th>Godkendt</th>
+    </tr>
+    </thead>
+</table>
+
+
+    @endsection
