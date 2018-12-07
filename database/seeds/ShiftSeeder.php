30a33a8c6909e6b86c6d65eda5e63b59195c1122
Anders <ande7195@edu.easj.dk>
2018-12-07 12:56:27 +0100

    shift seeder added

--- database/seeds/ShiftSeeder.php
+++ database/seeds/ShiftSeeder.php
@@ -10,20 +10,20 @@ class ShiftSeeder extends Seeder
      * @return void
      */
     public function run()
-    {	
-		for ($i=1; $i <= 100; $i++) { 
-			$random = random_int(1,30);
-			$shiftDay = Carbon::now()->addDays($random);
-			$endtime = Carbon::now()->addDays($random)->addHours(3);
-			DB::table('Shifts')->insert([
-				'date' => $shiftDay,
-				'tradeable' => random_int(0,1),
-				'startTime' => $shiftDay,
-				'endTime' => $endtime,
-				'FK_User' => random_int(1,40),
-				'created_at' => Carbon::now(),
-				'updated_at' => Carbon::now(),
-			]);
-		}
+    {
+        for ($i=1; $i <= 100; $i++) {
+            $random = random_int(1,30);
+            $shiftDay = Carbon::now()->addDays($random);
+            $endtime = Carbon::parse($shiftDay)->addHours(3);
+            DB::table('Shifts')->insert([
+                'date' => $shiftDay,
+                'tradeable' => random_int(0,1),
+                'startTime' => $shiftDay,
+                'endTime' => $endtime,
+                'FK_User' => random_int(1,40),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]);
+        }
     }
 }
