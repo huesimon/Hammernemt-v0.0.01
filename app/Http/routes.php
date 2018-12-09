82e5efcef1f90041a91f20b0344a8dd491bd55cf
Simon Rasmussen <simon.d.rasmussen@gmail.com>
2018-12-09 01:40:13 +0100

    route test

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -40,5 +40,4 @@ Route::get('auth/logout', 'Auth\AuthController@getLogout');
 Route::get('auth/register', 'Auth\AuthController@getRegister');
 Route::post('auth/register', 'Auth\AuthController@postRegister');
 Route::auth();
-
 Route::get('/home', 'HomeController@index');
