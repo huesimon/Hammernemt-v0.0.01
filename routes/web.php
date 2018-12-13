c80802b35d4864043d8df7734a8984ff55ce747b
Simon Rasmussen <simon.d.rasmussen@gmail.com>
2018-12-13 17:17:53 +0100

    added calendarcontroller

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,30 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/admin', 'AdminController@index');
+Route::get('/admin/tradelist', ['uses' =>'AdminController@tradeList', 'as'=>'adminTradeList']);
+Route::get('/admin/accept/{id}', ['uses' =>'AdminController@acceptTrade', 'as'=>'adminAcceptTrade']);
+
+Route::get('/calendar', ['uses' =>'CalendarController@index', 'as'=>'calendar']);
+
+Route::get('/calendar/mine/{userid}', ['uses' =>'CalendarController@myCalendar', 'as'=>'myCalendar']);
+
+Route::get('/shift', 'ShiftController@index');
+
+Route::get('/shift/all', ['uses' =>'ShiftController@all', 'as'=>'allShifts']);
+
+
+Route::get('/shift/{id}', ['uses' =>'ShiftController@show', 'as'=>'showShift']);
+
+
+Route::get('/shift/release/{id}', 'ShiftController@releaseInfo');
+
+Route::post('/shift/release/', 'ShiftController@releaseShift');
+
+Route::get('/tradelist', ['uses' =>'ShiftController@tradeList', 'as'=>'tradeList']);
+
+Route::get('/accept/{id}', ['uses' =>'ShiftController@acceptTrade', 'as'=>'acceptTrade']);
+
+Route::get('user/mystamps/{id}', 'UserController@myStamps');
+
+
