377b90dce70d253ee3cbe7bf099aaf7b7077bf54
Simon Rasmussen <simon.d.rasmussen@gmail.com>
2018-12-17 11:35:03 +0100

    admin approval view

---
+++ resources/views/admin/tradelist/approval.blade.php
@@ -0,0 +1,28 @@
+@extends('layout')
+
+@section('content')
+
+@foreach ($shiftTrades as $trade)
+	<div class="card" >
+		<div class="card-body">
+		<h4 class="card-title"> Fra: {{ $trade->getOriginalOwnerName()}}</h4>
+		<h4 class="card-title"> Til: {{ $trade->getNewOwnerName()}}</h4>
+		
+			<p class="card-text">
+				{!! $trade->getShift()->getTimeFormattedDateStartEndNewLine() !!}
+			</p>
+			@if (!is_null($trade->comment))
+				<p class="card-text">
+					Kommentar: {{$trade->comment}}		
+				</p>					
+			@endif
+
+			<a href={{route('adminAcceptTrade', ['id' => $trade->id] )}} class="btn btn-success"> Godkend vagt</a>
+		</div>
+	</div>
+
+
+@endforeach
+
+
+@endsection
