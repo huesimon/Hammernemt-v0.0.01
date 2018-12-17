9e8a334280c9ae61a8ead052c239d5569079717e
Anders <ande7195@edu.easj.dk>
2018-12-17 12:37:56 +0100

    UserStamp.php

--- app/UserStamp.php
+++ app/UserStamp.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserStamp extends Model
 {
-    //
+    public function scopeMyStamps($query, $userId=null){
+        $now=Carbon::now();
+        return $query->where('user_id', '=', $userId);
+    }
+    public function scopeMonthAndYear($query, $month, $year,$userId=null){
+        return $query->where('user_id', '=', $userId)->where('start_time');
+
+    }
+    private function getDate(){
+        $startTime =substr(UserStamp::find(1)->start_time, 0, 10);
+        dd($startTime);
+
+    }
+    public  function scopeStampByDate($query, $month, $year, $uid){
+        //SELECT * FROM `user_stamps` WHERE month(start_time)=11 and year(start_time)=2018
+        //SELECT * FROM `user_stamps` WHERE  month(start_time)=11 and year(start_time)=2018 and user_id=102
+
+    }
 }
