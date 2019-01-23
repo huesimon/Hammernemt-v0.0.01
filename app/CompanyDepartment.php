d5a45bf73c97e2cb6dfe7cb265978a4639a6bd9a
Jonas Ishøj Bauch <jib090994@gmail.com>
2019-01-23 15:38:48 +0100

    Update CompanyDepartment.php

--- app/CompanyDepartment.php
+++ app/CompanyDepartment.php
@@ -7,12 +7,5 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Support\Facades\DB;
 class CompanyDepartment extends Model
 {
-public function scopeMyDepartment($id){
-    $user = DB::table('users')->where('id', $id)->first();
-    $departmentId = $user->department_id;
-    $companyId = Company::CompanyId($id)->get();
-    $companyDepartment = DB::table('company_departments')->where('company_id',$companyId)->where('id',$departmentId)->first();
-    $myDepartment = $companyDepartment->name;
-    return $myDepartment;
-}
+
 }
